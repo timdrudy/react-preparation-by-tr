ae3d318dd4790e5cfa15fb44683cc56d7ebeca0b
Tim <timdrudy@gmail.com>
2025-05-02 11:49:00 -0500

    completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect,useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate(); 
+
+  async function getUsers() {
+    const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
-    <div>
-      Home
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          {users.map((user) => (
+            <div className="user" key={user.id} onClick={() => navigate(`${user.id}`)}>
+              <div className="user-card">
+                <div className="user-card__container">
+                  <h3>{user.name}</h3>
+                  <p>
+                    <b>Email:</b> {user.email}
+                  </p>
+                  <p>
+                    <b>Phone:</b> {user.phone}
+                  </p>
+                  <p>
+                    <b>Website:</b>
+                    {user.website}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Posts = () => {
+    let navigate = useNavigate();
     const { id } = useParams();
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState();
@@ -34,9 +35,7 @@ const Posts = () => {
   return (
 <>
   <div className="post__search">
-    <Link to="/">
-    <button>← Back</button>
-    </Link>
+    <button onClick={()=> navigate('/')}>← Back</button>
     <div className="post__search--container">
       <label className="post__search--label">Search by Id</label>
       <input
